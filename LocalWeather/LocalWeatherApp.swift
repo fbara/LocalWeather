e0738fc0f24d2ff1761a15a187b3f17586a93884
fbara <fbara1@yahoo.com>
2022-02-08 13:48:47 -0600

    Initial Commit

---
+++ LocalWeather/LocalWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalWeatherApp.swift
+//  LocalWeather
+//
+//  Created by Frank Bara on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct LocalWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
